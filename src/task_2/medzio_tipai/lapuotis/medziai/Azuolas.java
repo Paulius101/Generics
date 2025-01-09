28b7b717826186065b1f9a664c184f2c4ebe07bd
Paulius101 <p.dambrausko@gmail.com>
2025-01-09 11:43:39 +0200

    Medziai task first part implementation

---
+++ src/task_2/medzio_tipai/lapuotis/medziai/Azuolas.java
@@ -0,0 +1,11 @@
+package src.task_2.medzio_tipai.lapuotis.medziai;
+
+import src.task_2.medzio_tipai.lapuotis.Lapuotis;
+
+public class Azuolas extends Lapuotis {
+
+    @Override
+    public void turi() {
+        System.out.println("Azuolas turi lapus");
+    }
+}
